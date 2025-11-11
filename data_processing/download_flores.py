4ecab587f3f9e89aafab2095c285a5efa7f4edbf
Nathaniel Robinson <47231768+n8rob@users.noreply.github.com>
2025-11-11 10:23:51 -0500

    Switch to dev

--- data_processing/download_flores.py
+++ data_processing/download_flores.py
@@ -1,6 +1,9 @@
 import os
 from datasets import load_dataset
 
+SPLIT = 'dev' # can also be devtest, as in original AL-QASIDA paper
+# Do NOT set to devtest for AMIYA shared task
+
 DA_ISO_CODES = [ # Which Dialectal Arabic varieties to include
     'ary', # Moroccan Arabic 
     'arz', # Egyptian Arabic 
@@ -9,7 +12,7 @@ DA_ISO_CODES = [ # Which Dialectal Arabic varieties to include
     'ars', # Najdi (Saudi) Arabic 
 ]
 
-def get_lines(iso, script, split="devtest"): 
+def get_lines(iso, script, split=SPLIT): 
     # Example: get_lines('arz', 'Arab')
     flores_code = f"{iso}_{script}"
     data = load_dataset(
@@ -21,7 +24,7 @@ def get_lines(iso, script, split="devtest"):
     lines = [datum['sentence'].strip() + '\n' for datum in data] 
     return lines 
 
-def main(verbose=True, split="devtest"):
+def main(verbose=True, split=SPLIT):
     for iso in DA_ISO_CODES + ['arb', 'eng']: 
         # Retrieve data
         script = "Latn" if iso == "eng" else "Arab"
