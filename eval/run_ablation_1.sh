980839d904a8dfd645980b13f36ea6a52f263f7b
omar-kallas <omar.cpp@gmail.com>
2026-05-02 11:21:27 +0000

    clean up

--- eval/run_ablation_1.sh
+++
@@ -1,7 +0,0 @@
-python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c1_config.json" --task "monolingual" --dialects egy --out-dir ./llm_with_samples
-python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c2_config.json" --task "monolingual" --dialects egy --out-dir ./llm_with_samples
-python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c3_config.json" --task "monolingual" --dialects egy --out-dir ./llm_with_samples
-python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c4_config.json" --task "monolingual" --dialects egy --out-dir ./llm_with_samples
-python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c5_config.json" --task "monolingual" --dialects egy --out-dir ./llm_with_samples
-# python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c4_config.json" --task "mt" --dialects egy-eng egy-msa eng-egy msa-egy
-# python evaluator.py --data-dir "../data_processing/data" --llm "allam_steered_cairo" --use_config "configs/allam_steered_cairo_l16_c5_config.json" --task "mt" --dialects egy-eng egy-msa eng-egy msa-egy
